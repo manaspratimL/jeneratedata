f36961c420250b233b5d637056e698f4db44fdee
abarto <abarto@localhost>
2010-07-25 11:59:22 -0300

    Added comments to the initial and concatenating generator and removed public access to the concatenating generator generatos.

--- src/com/googlecode/jeneratedata/people/InitialGenerator.java
+++ src/com/googlecode/jeneratedata/people/InitialGenerator.java
@@ -4,7 +4,15 @@ import org.apache.commons.lang.RandomStringUtils;
 
 import com.googlecode.jeneratedata.core.Generator;
 
+/**
+ * Generates initials like A., B., etc.
+ * 
+ * @author Agustin Barto <abarto@gmail.com>
+ */
 public class InitialGenerator implements Generator<String> {
+	/* (non-Javadoc)
+	 * @see com.googlecode.jeneratedata.core.Generator#generate()
+	 */
 	@Override
 	public String generate() {
 		return RandomStringUtils.randomAlphabetic(1).toUpperCase() + ".";
--- src/com/googlecode/jeneratedata/text/ConcatenatingGenerator.java
+++ src/com/googlecode/jeneratedata/text/ConcatenatingGenerator.java
@@ -5,27 +5,43 @@ import java.util.Collection;
 
 import com.googlecode.jeneratedata.core.Generator;
 
+/**
+ * Generates a String concatenating the generated values by the contained
+ * {@link Generator} instances. 
+ * 
+ * @author Agustin Barto <abarto@gmail.com>
+ */
 public class ConcatenatingGenerator implements Generator<String> {
-	private Collection<Generator<?>> generators;
+	/**
+	 * The generators used to construct the data item.
+	 */
+	protected Collection<Generator<?>> generators;
 	
+	/**
+	 * Constructor.
+	 * 
+	 * @param generators Generators to be used to construct this generator data
+	 * item.
+	 */
 	public ConcatenatingGenerator(Generator<?>... generators) {
 		super();
 		this.generators = Arrays.asList(generators);
 	}	
 
+	/**
+	 * Constructor.
+	 * 
+	 * @param generators Generators to be used to construct this generator data
+	 * item.
+	 */
 	public ConcatenatingGenerator(Collection<Generator<?>> generators) {
 		super();
 		this.generators = generators;
 	}
 
-	public Collection<Generator<?>> getGenerators() {
-		return generators;
-	}
-
-	public void setGenerators(Collection<Generator<?>> generators) {
-		this.generators = generators;
-	}
-
+	/* (non-Javadoc)
+	 * @see com.googlecode.jeneratedata.core.Generator#generate()
+	 */
 	@Override
 	public String generate() {
 		StringBuffer stringBuffer = new StringBuffer();
