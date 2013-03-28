bfab8359c9af1bb7e55e1bdb365c1fc0f913dd88
Agustin Barto <abarto@gmail.com>
2013-03-28 12:10:43 -0300

    Added missing constructors to ConcatenatingGenerator

--- src/com/googlecode/jeneratedata/text/ConcatenatingGenerator.java
+++ src/com/googlecode/jeneratedata/text/ConcatenatingGenerator.java
@@ -1,15 +1,36 @@
 package com.googlecode.jeneratedata.text;
 
+import java.util.Collection;
+
 import com.googlecode.jeneratedata.core.Generator;
 import com.googlecode.jeneratedata.core.GeneratorCollectionWrapperBase;
 
 /**
- * Generates a String concatenating the generated values by the contained
- * {@link Generator} instances. 
- * 
+ * Generates a String concatenating the generated values by the contained.
+ *
+ * {@link Generator} instances.
  * @author Agustin Barto <abarto@gmail.com>
  */
 public class ConcatenatingGenerator extends GeneratorCollectionWrapperBase implements Generator<String> {
+	
+	/**
+	 * Instantiates a new concatenating generator.
+	 *
+	 * @param generators the generators
+	 */
+	public ConcatenatingGenerator(Collection<Generator<?>> generators) {
+		super(generators);
+	}
+
+	/**
+	 * Instantiates a new concatenating generator.
+	 *
+	 * @param generators the generators
+	 */
+	public ConcatenatingGenerator(Generator<?>... generators) {
+		super(generators);
+	}
+
 	/* (non-Javadoc)
 	 * @see com.googlecode.jeneratedata.core.Generator#generate()
 	 */
